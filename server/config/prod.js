fd332babc5bec8b2d67c38455c1aa127386faf57
mail2mz <mail2mz@gmail.com>
2020-06-05 14:59:51 -0400

    add uploads folder

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:process.env.MONGO_URI
+   //* mongoURI:process.env.MONGO_URI
+   mongoURI:'mongodb+srv://user1:user1@clusterone-n3x4p.mongodb.net/test?retryWrites=true&w=majority'
 }
